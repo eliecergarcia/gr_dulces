e0dc147aba20544811e69ae471d0fc8514e3405d
Absa Garcia <55288682+absaGarcia@users.noreply.github.com>
2021-03-02 19:14:31 -0600

    se agrego provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quiero_dulces/pages/categories_page.dart';
+import 'package:quiero_dulces/pages/category_selected.dart';
 import 'package:quiero_dulces/pages/login_page.dart';
+
+import 'objects/cart_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,18 +14,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'AvenirLTstd',
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CartModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'AvenirLTstd',
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: LoginPage.id,
+        routes: {
+          LoginPage.id: (_) => LoginPage(),
+          CategoriesPage.id: (_) => CategoriesPage(),
+          CategorySelectedPage.id: (_) => CategorySelectedPage(),
+        },
       ),
-      initialRoute: LoginPage.id,
-      routes: {
-        LoginPage.id: (_) => LoginPage(),
-        CategoriesPage.id: (_) => CategoriesPage(),
-      },
     );
   }
 }
---
+++ lib/pages/category_selected.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategorySelectedPage extends StatelessWidget {
+  static String id = 'category_selected';
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        
+      ),
+    );
+  }
+}
